869242bdebfcddf15365788919d23fd405a7f3f7
Nikola <radanovicnikola93@gmail.com>
2018-01-30 21:10:41 +0100

    Added functions and variable

--- supermarket.py
+++ supermarket.py
@@ -22,3 +22,15 @@ for food in prices:
   print prices[food] * stock[food]
   total = total + prices[food] * stock[food]
 print total
+
+
+shopping_list = ["banana", "orange", "apple"]
+
+def compute_bill(food):
+  total = 0
+  for item in food:
+    if stock[item] > 0:
+      total = total + prices[item]
+      stock[item] = stock[item] - 1
+  return total
+
